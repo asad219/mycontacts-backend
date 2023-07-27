7746733f6c15b954cac38bdc88d79b5be2282b33
Asad Ullah Khan <asad219@hotmail.com>
2023-07-27 12:18:37 +0400

    contact model connected

--- routes/contactRoutes.js
+++ routes/contactRoutes.js
@@ -1,24 +1,18 @@
 const express = require("express");
 const router = express.Router();
+const {
+  getContacts,
+  getContact,
+  createContact,
+  updateContact,
+  deleteContact,
+} = require("../controllers/contactController");
 
-router.route("/").get((req, res)=>{
-    res.status(200).json({message: "Get all contacts"});
-});
+//**** GET ALL CONTACTS AND CREATE CONTACT BECAUSE ROUTE IS SAME SINGLE SLASH "/" */
+router.route("/").get(getContacts).post(createContact);
 
-router.route("/:id").get((req, res)=>{
-    res.status(200).json({message: `Get contact for ${req.params.id}`});
-});
+//**** GET, UPDATE, DELETE SINGLE CONTACT BECAUSE ROUTE IS SAME SINGLE SLASH WITH ID PARAM "/:id" */
+router.route("/:id").get(getContact).put(updateContact).delete(deleteContact);
 
-router.route("/").post((req, res)=>{
-    res.status(200).json({message: "Create contacts"});
-});
-
-router.route("/:id").put((req, res)=>{
-    res.status(200).json({message: `Update contact for  ${req.params.id}`});
-});
-
-router.route("/:id").delete((req, res)=>{
-    res.status(200).json({message: `delete contact for ${req.params.id}`});
-});
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,12 +1,18 @@
 const express = require("express");
 const dotenv = require("dotenv").config();
 const contact = require("./routes/contactRoutes");
+const errorHandler = require("./middleware/errorHandler");
+const connectDb = require("./config/dbConnection");
+
+connectDb();
 
 const app = express();
 
 const port = process.env.PORT || 5000;
 
+app.use(express.json());
 app.use("/api/contacts", contact);
+app.use(errorHandler);
 
 app.listen(port, ()=>{
     console.log(`Server running on port: ${port}`);
