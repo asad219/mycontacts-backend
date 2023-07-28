4f218b9158ff1fbd779990effb67e8f80038883e
Asad Ullah Khan <asad219@hotmail.com>
2023-07-28 12:10:18 +0400

    user route start

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const dotenv = require("dotenv").config();
 const contact = require("./routes/contactRoutes");
+const users = require("./routes/userRoutes");
 const errorHandler = require("./middleware/errorHandler");
 const connectDb = require("./config/dbConnection");
 
@@ -12,6 +13,7 @@ const port = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use("/api/contacts", contact);
+app.use("/api/contacts", users);
 app.use(errorHandler);
 
 app.listen(port, ()=>{
